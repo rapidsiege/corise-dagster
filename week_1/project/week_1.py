e82da63abc2fe3303113ea3ce59f65391bf7234b
= <=>
2023-04-09 17:18:41 +0200

    week 1 project

--- week_1/project/week_1.py
+++ week_1/project/week_1.py
@@ -50,26 +50,51 @@ def csv_helper(file_name: str) -> Iterator[Stock]:
             yield Stock.from_list(row)
 
 
-@op
-def get_s3_data_op():
+@op(
+    description = "Returns list of stock data from s3_key",
+    config_schema = {"s3_key": str},
+    out = {"stocks": Out(dagster_type = List[Stock],
+        description = "List of stocks")}
+)
+def get_s3_data_op(context) -> List[Stock]:
+    file_name = context.op_config["s3_key"]
+    return list(csv_helper(file_name))
+
+
+@op(
+    description = "Returns the Aggregation from the stock value with the greatest high value",
+    ins = {"stocks": In(dagster_type = List[Stock],
+        description = "List of stocks")},
+    out = {"stock_greatest_high": Out(dagster_type = Aggregation,
+        description = "Date and high values from the stock value with the greatest high value")}
+)
+def process_data_op(context, stocks) -> Aggregation:
+    max_high_value = max(stocks, key = lambda x: x.high)
+    return Aggregation(date = max_high_value.date, high = max_high_value.high)
+
+
+@op(
+    description = "Currently unused. Will upload stock_greatest_high (Aggregation class type) to Redis",
+    ins = {"stock_greatest_high": In(dagster_type = Aggregation,
+        description = "Date and high values from the stock value with the greatest high value")}
+)
+def put_redis_data_op(context, stock_greatest_high) -> Nothing:
     pass
 
 
-@op
-def process_data_op():
+@op(
+    description = "Currently unused. Will upload stock_greatest_high (Aggregation class type) to s3 data lake",
+    ins = {"stock_greatest_high": In(dagster_type = Aggregation,
+        description = "Date and high values from the stock value with the greatest high value")}
+)
+def put_s3_data_op(context, stock_greatest_high) -> Nothing:
     pass
 
 
-@op
-def put_redis_data_op():
-    pass
-
-
-@op
-def put_s3_data_op():
-    pass
-
-
-@job
+@job(
+    config = {"ops": {"get_s3_data_op": {"config": {"s3_key": "week_1/data/stock.csv"}}}},
+    description = "Currently passing .csv filepath as s3_key so that the job works in dagit"
+)
 def machine_learning_job():
-    pass
+    put_redis_data_op(process_data_op(get_s3_data_op()))
+    ##put_s3_data_op(process_data_op(get_s3_data_op()))
